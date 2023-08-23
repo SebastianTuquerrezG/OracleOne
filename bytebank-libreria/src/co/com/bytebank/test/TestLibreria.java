1914429b0a309e6a02c27b26e5a01c1fb1233a9c
Sebastian Tuquerrez <jtuquerrez@unicauca.edu.co>
2023-08-23 01:00:17 -0500

    Usando mi proyecto como libreria en otro

---
+++ bytebank-libreria/src/co/com/bytebank/test/TestLibreria.java
@@ -0,0 +1,10 @@
+package co.com.bytebank.test;
+
+import co.com.bytebank.modelo.Cuenta;
+import co.com.bytebank.modelo.CuentaAhorros;
+
+public class TestLibreria {
+    public static void main(String[] args) {
+        CuentaAhorros c = new CuentaAhorros(1,2);
+    }
+}
